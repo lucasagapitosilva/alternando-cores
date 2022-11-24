9094fb7429240b7800714c1e23611d3488128700
Lucas Agapito <lucasbezerra.rocinha@gmail.com>
2022-11-24 15:24:53 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [novaCor, setNovaCor] = useState('bg-vermelho')
+
+  const alterarCor = () => {
+
+    if(novaCor === 'bg-vermelho') {
+      setNovaCor('bg-azul')
+    } else {
+      setNovaCor('bg-vermelho')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='container'>
+      <div className="App">
+
+        <h2>Alterando estilo do elemento</h2>
+
+        <div className={novaCor}></div>
+
+        <p>Clique no botão para alterar o estilo do elemento acima.</p>
+        <div className='botao'>
+          <button onClick={alterarCor}>Alterar</button>
+        </div>
+      </div>
     </div>
   );
 }
